d1802a8f646c68e918e57e0d2797cc41d1112b6a
chaar99 <carmenlopezcalvo.4c@gmail.com>
2020-10-02 10:17:53 +0200

    subiendo statico + especificacion de cada post

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+#<int:pk>- esta parte es más complicada. Significa que Django 
+# espera un valor entero y lo transferirá a una vista como una variable llamada pk.
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
